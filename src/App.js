848dc7c8f3a11eb46b6c988041e3deca5327c523
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-03 11:24:57 -0500

    added navigation
    works fine! =)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Navigation from './components/Navigation'
+
 import './style.scss'
 
 export default function App() {
     return (
         <div className="App">
-            <h1>hey from app</h1>
+            <Navigation />
         </div>
     )
 }
